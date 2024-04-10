e2b54899e91f66abe64c16053def57fdb4f92e99
Tom Bury <tom.bury@hotmail.com>
2024-04-10 22:39:37 +0200

    feat: add git_switch_remote function

--- git/.zshrc_git-switch.sh
+++ git/.zshrc_git-switch.sh
@@ -21,4 +21,26 @@ git_switch() {
   fi
 }
 
+git_switch_remote() {
+  if [ "$1" = "-" ]; then
+    # switch to the branch you were on before
+    git switch -
+  elif git show-ref --verify --quiet refs/remotes/"$1"; then
+    # switch to the branch you specified
+    git switch "$1"
+  else
+    # switch to the branch you select with fzf
+    git switch $(
+        git branch -r | 
+        fzf \
+            --color='prompt:#af5fff,header:#262626,gutter:-1,pointer:#af5fff' \
+            --reverse \
+            --pointer="⏺" \
+            --query="$1" | 
+        tr -d '[:space:]'
+    )
+  fi
+}
+
 alias gitsw=git_switch
+alias gitswr=git_switch_remote
