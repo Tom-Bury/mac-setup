21c10660b0021a1dc439886ea5250d9b109eeed1
Tom Bury <tom.bury@hotmail.com>
2024-04-23 09:25:35 +0200

    feat: add fzf cd command

---
+++ shell/.zshrc_cd_fzf.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env zsh
+
+IGNORE_DIRS=(
+    node_modules
+    .git
+    build
+    dist
+    .bundle
+    local-maven-repository
+    Pods
+    .gradle
+)
+
+fd() {
+    local dir
+    local ignore_expr
+    for ignore_dir in "${IGNORE_DIRS[@]}"; do
+        ignore_expr="${ignore_expr} -not -path '*/${ignore_dir}/*'"
+    done
+    
+    dir=$(eval find . -maxdepth 3 -type d $ignore_expr | fzf --prompt='CD into > ' \
+                --color='prompt:#af5fff,header:#262626,gutter:-1,pointer:#af5fff' \
+                --reverse \
+                --pointer="⏺" \
+        )
+    if [[ -n $dir ]]; then
+        cd "$dir"
+    fi
+}
